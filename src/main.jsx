083679232663fa304067bc4c60ad9eb66d4a2041
Mir Musaib <meermusaib20@gmail.com>
2026-02-13 06:39:09 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio_">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
